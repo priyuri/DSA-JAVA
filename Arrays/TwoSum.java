dc5cadc8476adda163a0436e13f57613a68f345a
priyuri <155281977+priyuri@users.noreply.github.com>
2026-07-19 08:29:42 +0530

    Implement twoSum method to find indices of target

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int  right = nums.length-1;
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            if(sum==target){
+                return new int[]{left , right};
+            }else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[]{};
+        
+    }
+    public static void main(String args[]){
+        int[] nums = {2,7,11,15};
+        int target=9;
+        int[] result = twoSum(nums,target);
+        if(result.length==0){
+            System.out.println("null");
+        }else{
+            System.out.println(result[0]+" "+result[1]);
+        }
+}
+}
