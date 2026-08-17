53120d92c63c048cea5e0f63eb8bc7d4b070c019
priyuri <mayuridurge04@gmail.com>
2026-08-17 17:06:53 +0530

    better approach implemented

--- Arrays/palindrome.java
+++ Arrays/palindrome.java
@@ -1,26 +1,40 @@
 public class palindrome {
 
     public static boolean isPalindrome(int x) {
-        String y = String.valueOf(x);
-        int left = 0;
-        int right = y.length() - 1;
-        while (left < right) {
-            char leftChar = y.charAt(left);
-            char rightChar = y.charAt(right);
-            if (leftChar != rightChar) {
-                return false;
-            }
-            left++;
-            right--;
-
+        int original = x;
+        int reverse = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
         }
-        return true;
+        if (original == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+
+        // string method
+        // String y = String.valueOf(x);
+        // int left = 0;
+        // int right = y.length() - 1;
+        // while (left < right) {
+        // char leftChar = y.charAt(left);
+        // char rightChar = y.charAt(right);
+        // if (leftChar != rightChar) {
+        // return false;
+        // }
+        // left++;
+        // right--;
+
+        // }
+        // return true;
 
     }
 
     public static void main(String[] args) {
         int x = 10;
-        
+
         System.out.println(isPalindrome(x));
     }
 
