004580ce160f0bdf69dcae2b698ea723b3487dca
priyuri <mayuridurge04@gmail.com>
2026-08-18 19:37:32 +0530

    missing number problem

---
+++ Arrays/missingNum.java
@@ -0,0 +1,17 @@
+public class missingNum {
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedValue = n*(n+1)/2;
+        int actualValue = 0;
+        for(int i=0; i<nums.length; i++){
+           actualValue = actualValue + nums[i]; 
+        }
+        return expectedValue - actualValue;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {0,1};
+        int ans = missingNumber(nums);
+        System.out.println(ans);
+    }
+}
